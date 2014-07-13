759ec512b99c8427445d1414f1fa2db6d0db4e15
Trevor Polischuk <trevorpolischuk@gmail.com>
2014-07-13 11:52:20 -0700

    Removed uglify, added qunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,7 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        },
+
         concat: {
             options: {
                 separator: ';'
@@ -25,13 +17,16 @@ module.exports = function(grunt) {
                     }
                 ]
             }
+        },
+        qunit: {
+            all: ['runners/main.html']
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['qunit']);
 
 };
