2fb68acf812577428f5d13f8acd0d94e2dacd50d
Trevor Polischuk <trevorpolischuk@gmail.com>
2014-08-24 21:31:02 -0700

    Hooking up the JS compile targets

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,6 +61,6 @@ module.exports = function(grunt) {
 
     grunt.registerTask('test', ['jshint', 'qunit']);
 
-    grunt.registerTask('default', ['jshint', 'less', 'concat', 'uglify', 'watch']);
+    grunt.registerTask('default', ['jshint', 'less', 'concat', 'watch']);
 
 };
